659cd8c3ebd6ee1613621a9dd2e2fbacd72b9ecc
Deshraj <deshrajdry@gmail.com>
2015-08-14 02:20:29 +0530

    django project started

---
+++ evm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for evm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "evm.settings")
+
+application = get_wsgi_application()
