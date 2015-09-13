7e4edf90d6136b7ca8fdc5c8cd2a34f902f16b35
Deshraj <deshrajdry@gmail.com>
2015-09-13 16:19:18 +0530

    Attendee models created

--- attendees/models.py
+++ attendees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.user.models import User
 
 # Create your models here.
+
+class Attendee(models.Model):
+	attendee = models.ForeignKeyField(User)
+	address = models.CharField(max_length = 1000, default = None)
+	profession = models.CharField()
+	phone_number = models.PositiveIntegerfield(default = None)
