5e87fc8baef6c99c709688a94a9a187f422f0b24
Deshraj <deshrajdry@gmail.com>
2015-09-13 16:20:29 +0530

    Url for events and attendees added

--- evm/urls.py
+++ evm/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import events, attendees
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'evm.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'evm.views.home', name='home'),
+    url(r'^events/', include('events.urls')),
+    url(r'^attendees/', include('attendees.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
