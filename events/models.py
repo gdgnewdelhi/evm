f7caf6fd08792125a8a546ee6834acc3e9daefed
Deshraj <deshrajdry@gmail.com>
2015-09-13 16:19:35 +0530

    Event model created

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Event(models.Model):
+	name = models.CharField(max_length = 200, default = None)
+	title = models.CharField(max_length = 200, default = None)
+	description = models.CharField(max_length = 5000, default = None)
+	start_date_time = models.DateTimeField(default = None)
+	end_date_time = models.DateTimeField(max_length = 200, default = None)
+	no_of_attendee = models.PositiveIntegerField(max_length = 200, default = None)
+	speaker = models.ForeignKeyField(User)
+
