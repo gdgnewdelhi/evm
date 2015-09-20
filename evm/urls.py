2f97695d05e64a32aa7486f37ee0578fa88464fe
Deshraj <deshrajdry@gmail.com>
2015-09-20 22:53:40 +0530

    url config modified

--- evm/urls.py
+++ evm/urls.py
@@ -4,9 +4,9 @@ import events, attendees
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'evm.views.home', name='home'),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^$', 'events.views.home', name='home'),
     # url(r'^events/', include('events.urls')),
     # url(r'^attendees/', include('attendees.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('allauth.urls')),
 ]
