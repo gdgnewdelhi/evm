14e578123303ed4ebfe2f5d631809c1b8642e502
Deshraj <deshrajdry@gmail.com>
2015-09-20 19:28:59 +0530

    url config for django allauth set

--- evm/urls.py
+++ evm/urls.py
@@ -5,7 +5,8 @@ import events, attendees
 urlpatterns = [
     # Examples:
     url(r'^$', 'evm.views.home', name='home'),
-    url(r'^events/', include('events.urls')),
-    url(r'^attendees/', include('attendees.urls')),
+    # url(r'^events/', include('events.urls')),
+    # url(r'^attendees/', include('attendees.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('allauth.urls')),
 ]
